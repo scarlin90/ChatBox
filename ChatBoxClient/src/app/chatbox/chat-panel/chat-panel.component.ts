7d282dbacefa51f51cd8ed132dfe9af014dffa40
Sean Carlin <scarlin@smashfly.com>
2019-04-07 16:46:48 +0100

    Adding login styling and dashboard styling

--- ChatBoxClient/src/app/chatbox/chat-panel/chat-panel.component.ts
+++ ChatBoxClient/src/app/chatbox/chat-panel/chat-panel.component.ts
@@ -26,8 +26,6 @@ export class ChatPanelComponent implements OnInit {
       sender: '',
       timestamp: new Date()
     })
+    this.messageBody = '';
   }
-
-  this.messageBody = '';
-
 }
--- ChatBoxClient/src/app/home/home.component.ts
+++ ChatBoxClient/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { HubConnection } from '@aspnet/signalr';
 import * as signalR from '@aspnet/signalr';
+import { UserService } from '../shared/user/user.service';
+import { User } from '../shared/user/user.model';
 
 @Component({
   selector: 'app-home-component',
@@ -9,30 +11,12 @@ import * as signalR from '@aspnet/signalr';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    // private _hubConnection: HubConnection | undefined;
-    // public async: any;
-    // message = '';
-    // messages: string[] = [];
- 
-    constructor() {
-    }
- 
-    // public sendMessage(): void {
-    //     if (this._hubConnection) {
-    //         this._hubConnection.invoke('SendMessage', 'test user', this.message);
-    //     }
-    // }
- 
-    ngOnInit() {
-        // this._hubConnection = new signalR.HubConnectionBuilder()
-        //     .withUrl('http://localhost:5000/chatbox')
-        //     .configureLogging(signalR.LogLevel.Information)
-        //     .build();
 
-        // this._hubConnection.start().catch(err => console.error(err.toString()));
-        // this._hubConnection.on('SendMessage', (user: any, message: any,) => {
-        //     const received = `${user}: ${message} `;
-        //     this.messages.push(received);
-        // });
+    public loggedInUser: User;
+
+    constructor(private _userService:UserService){}
+
+    ngOnInit() {
+        this.loggedInUser = this._userService.getLoginUser();
     }
 }
